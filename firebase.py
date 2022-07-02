32421133f368be60fe8731d2ffef17ca4e6b06c7
YutoShinohara <yuto.shino0128@gmail.com>
2022-07-02 14:36:11 +0900

    七月二日の成果

--- firebase.py
+++ firebase.py
@@ -1,49 +1,46 @@
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-#from firebase_admin import storage
-from google.cloud import storage
-import os
-
-GOOGLE_APPLICATION_CREDENTIALS = "/Users/yuto_shinohara/Documents/Programming/python/textdata/python firebase/loadimage-9bac0-firebase-adminsdk-abrf6-6458eb0b50.json"
-FIREBASE_STORAGE_BUCKET = "loadimage-9bac0.appspot.com"
-
-cred = credentials.Certificate(GOOGLE_APPLICATION_CREDENTIALS)
-firebase_admin.initialize_app(cred,{'storageBucket':FIREBASE_STORAGE_BUCKET})
-#bucket = storage.bucket()
-
-def upload_blob(source_file_name, destination_blob_name):
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(FIREBASE_STORAGE_BUCKET)
-    blob = bucket.blob(destination_blob_name)
-    blob.upload_from_filename(source_file_name)
-    print(
-        "File {} uploaded to {}.".format(
-            source_file_name, destination_blob_name
-        )
-    )
-def download_blob(source_blob_name, destination_file_name):
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(FIREBASE_STORAGE_BUCKET)
-    blob = bucket.blob(source_blob_name)
-    blob.download_to_filename(destination_file_name)
-    print(
-        "Blob {} downloaded to {}.".format(
-            source_blob_name, destination_file_name
-        )
-    )
-
-def blob_list(prefix=""):
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(FIREBASE_STORAGE_BUCKET)
-    blobs = list(bucket.list_blobs(prefix=prefix))
-    print(
-        "Blob list to {}.".format(
-            blobs
-        )
-    )
-    return blobs
+from firebase_admin import credentials,initialize_app,storage
+import urllib.request
+from urllib.parse import urlencode
 
 
-if __name__ == "__main__":
-    download_blob(source_blob_name, destination_file_name)
+#firebase認証
+cred = credentials.Certificate("loadimage-9bac0-firebase-adminsdk-abrf6-698df0bf5f.json")
+initialize_app(cred,{'storageBucket': 'gs://loadimage-9bac0.appspot.com'})
+
+bucket = storage.bucket('loadimage-9bac0.appspot.com')
+#アップロード用
+filenameup = 'hanba-gu6321.png'
+path = '/Users/yuto_shinohara/Documents/Programming/python/textdata/python firebase/'
+#ダウンロード用
+filenamedown = "Item"
+
+#urlクエリパラメータ
+q_data = {
+    'q': 'Pythonでクエリパラメータを作成',
+    'option': [1, 3, 5],
+    'lang': 'jp',
+}
+
+#アップロード
+def upload_blob(filenameup, path):
+    blob = bucket.blob(filenameup)
+    blob.upload_from_filename(path + filenameup)
+    blob.make_public()
+    print(blob.public_url)  
+
+#ダウンロード
+def download_blob(filenamedown, path):
+    blob = bucket.blob(filenamedown)
+    blob.download_to_filename(path + filenamedown)
+    blob.make_public()
+    print(blob.public_url) 
+    # qs = urlencode(q_data)
+    # firebasedownloadurl = blob.public_url + qs
+    # print(firebasedownloadurl)
+    urllib.request.urlretrieve(blob.public_url)
+    print("ダウンロード完了")
+    
+#実行
+if __name__ == '__main__':
+    download_blob(filenamedown,path)
+
